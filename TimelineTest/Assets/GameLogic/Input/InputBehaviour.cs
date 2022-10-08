dd4a6163176706816b8e82018e5fd9b8fdb69216
windsmoon <smwindsmoon@gmail.com>
2022-10-08 15:45:26 +0800

    init project

---
+++ TimelineTest/Assets/GameLogic/Input/InputBehaviour.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Windsmoon.GameLogic.Input
+{
+    public class InputBehaviour : MonoBehaviour
+    {
+        #region delegates
+        #endregion
+
+        #region fields
+        #endregion
+
+        #region methods
+        #endregion
+    }
+}
+  
