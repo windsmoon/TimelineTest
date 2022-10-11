6a3f10ea2a2875b2fbe149d39f09a629982aae28
windsmoon <smwindsmoon@gmail.com>
2022-10-11 11:39:21 +0800

    update

--- TimelineTest/Assets/GameLogic/PlayableExtensions/VignetteControlTrack.cs
+++ TimelineTest/Assets/GameLogic/PlayableExtensions/VignetteControlTrack.cs
@@ -8,7 +8,7 @@ namespace GameLogic.PlayableExtensions
     [TrackBindingType(typeof(Volume))]
     [TrackClipType(typeof(VignetteControlClip))]
     [TrackColor(0.32f, 0.21f,0.55f)]
-    public class VignettControlTrack : TrackAsset
+    public class VignetteControlTrack : TrackAsset
     {
         #region methods
         public override Playable CreateTrackMixer(PlayableGraph graph, GameObject go, int inputCount)
